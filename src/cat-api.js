494e1760e36755cef5dd32ac3c3e6d7d4fe63693
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-10-22 13:20:01 +0300

    add test

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,48 @@
+import axios from "axios";
 
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_J8vcnhGDQcx0EzpCBnkYsWplubKDkBqkaheBllTg73uOxKEXNJJiMt1EPpkDz43c';
+
+const refs = {
+    selector: document.querySelector('.breed-select'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+    divCatInfo: document.querySelector('.cat-info'),
+}
 
 export function fetchBreeds() {
 
-    return fetch(`${BASE_URL}/breeds?key=${API_KEY}`)
-        .then(resp => {
-            // console.log(resp);
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
+    // return fetch(`${BASE_URL}/breeds?key=${API_KEY}`)
+    //     .then(resp => {
+    //         // console.log(resp);
+    //         if (!resp.ok) {
+    //             throw new Error(resp.statusText);
+    //         }
 
-            return resp.json()
+    //         return resp.json()
+    // })
+
+    axios
+        .get(`${BASE_URL}/breeds?key=${API_KEY}`)
+        .then(resp => {
+            console.log(resp);
+            const breedMarkup = resp.data
+                .map(({ id, name }) => {
+                    return `optiom value = ${id}>${name}</option>`;
+                }).join('');
+            refs.selector.insertAdjacentHTML('beforeend', breedMarkup);
+        })
+        .catch(err => {
+            console.log(err.message);
     })
     }
 
 
 export function fetchCatByBreed(breedId) {
-
-    return fetch(`${BASE_URL}/breeds?key=${API_KEY}&breed_ids=${breedId}`)
+return axios
+   .get(`${BASE_URL}/images/search?key=${API_KEY}&breed_ids=${breedId}`)
         .then(resp => {
-            console.log(resp);
+            
             if (!resp.ok) {
                 throw new Error(resp.statusText);
             }
@@ -26,4 +50,3 @@ export function fetchCatByBreed(breedId) {
             return resp.json()
     })
     }
-console.log(fetchBreeds);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 import axios from "axios";
 import SlimSelect from 'slim-select'
-
+import Notiflix from 'notiflix';
 
 const refs = {
     selector: document.querySelector('.breed-select'),
@@ -12,8 +12,61 @@ const refs = {
 
 const { selector, loader, error, divCatInfo } = refs;
 
-loader.classList.replace('.loader', 'is-hidden');
-error.classList.add('is-hidden');
-divCatInfo.classList.add('is-hidden');
+// loader.classList.replace('.loader', 'is-hidden');
+// error.classList.add('is-hidden');
+// divCatInfo.classList.add('is-hidden');
 
-let arrBreedsId = [];
+
+// refs.selector.addEventListener('change', createMarkup);
+
+
+fetchBreeds()
+.then(data => {
+    return data.map(({id, name}) => ({text: name, value: id}));
+})
+        new SlimSelect({
+        select: selector,
+        data: arrBreedsId
+    })
+
+    .catch(onFetchError);
+
+selector.addEventListener('change', onSelectBreed);
+
+function onSelectBreed(event) {
+    loader.classList.replace('is-hidden', 'loader');
+    selector.classList.add('is-hidden');
+    divCatInfo.classList.add('is-hidden');
+
+    const breedId = event.currentTarget.value;
+    fetchCatByBreed(breedId)
+    .then(data => {
+        loader.classList.replace('loader', 'is-hidden');
+        selector.classList.remove('is-hidden');
+        const { url, breeds } = data[0];
+        
+        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
+        divCatInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+};
+
+function onFetchError(error) {
+    selector.classList.remove('is-hidden');
+    loader.classList.replace('loader', 'is-hidden');
+
+    // Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+    //     position: 'center-center',
+    //     timeout: 5000,
+    //     width: '400px',
+    //     fontSize: '24px'
+    // });
+};
+   
+
+
+
+
+
+
+    
