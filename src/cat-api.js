74d9ffa65c98b64d20aa702d46e9902d6de513da
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-10-23 00:16:44 +0300

    add end

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,30 +1,15 @@
 import axios from "axios";
-
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY = 'live_J8vcnhGDQcx0EzpCBnkYsWplubKDkBqkaheBllTg73uOxKEXNJJiMt1EPpkDz43c';
 
-
-
 export function fetchBreeds() {
-
-
    return axios
         .get(`${BASE_URL}/breeds?key=${API_KEY}`)
         .then(resp => resp.data)
         
     }
     
-
-
 export function fetchCatByBreed(breedId) {
 return axios
    .get(`${BASE_URL}/images/search?key=${API_KEY}&breed_ids=${breedId}`)
-        .then(resp => {
-            
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
-
-            return resp.json()
-    })
-    }
+        .then(resp =>  resp.data)}
--- src/index.js
+++ src/index.js
@@ -1,56 +1,35 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-
 import SlimSelect from 'slim-select'
+import "slim-select/dist/slimselect.css"
 import Notiflix from 'notiflix';
-
 const refs = {
     selector: document.querySelector('.breed-select'),
     loader: document.querySelector('.loader'),
     error: document.querySelector('.error'),
     divCatInfo: document.querySelector('.cat-info'),
 }
-
 const { selector, loader, error, divCatInfo } = refs;
-
 refs.loader.style.visibility = 'hidden';
 refs.error.style.visibility = 'hidden';
-
-
-
-
-
-fetchBreeds()
-    .then(data => {
-        // console.log(data);
-        refs.selector.insertAdjacentHTML("beforeend", breedMarkup);
-
-        return data.map(({ id, name }) => ({ text: name, value: id }));
-        
-        new SlimSelect({
-            select: selector,
-        
-            
+const select =  new SlimSelect({
+    select: "#select",
 })
-    }).catch(onFetchError);
-
-function breedMarkup(arr) {
-    return arr
-        .map(({ id, name }) => `<option value = "${id}">${name}</option>`).join('');
-}
-
-
+    fetchBreeds()
+    .then(data => {
+        const options = data.map(({ id, name }) => ({ text: name, value: id }))
+        select.setData(options);
+    })
+    .catch(onFetchError)
 refs.selector.addEventListener('change', selectCat);
-
 
 function selectCat(event) {
     event.preventDefault();
     refs.divCatInfo.innerHTML = '';
 
     let breedId = event.currentTarget.value;
-    refs.loader.style.visibility = 'visible';
+    refs.loader.style.visibility = 'hidden';
     refs.divCatInfo.style.visibility = 'hidden';
     
-
     fetchCatByBreed(breedId)
         
         .then(data => {
@@ -63,12 +42,30 @@ function selectCat(event) {
             
            
         })
-    
-}
+     fetchBreeds()
+        .then(data => {
+            const selectedBreed = data.find(breed => breed.id === breedId);
+            
+           
+            const infoDescription = selectedBreed.description;
+            const infoTemperament = selectedBreed.temperament;
+            const infoName = selectedBreed.name;
+            
+            const infoAboutCat = `<div class="cat-container">
+                         <h1>${infoName}</h1>
+                         <p>${infoDescription}</p>
+                        <p><b>Temperament:</b> ${infoTemperament}</p>` 
+            
+            refs.divCatInfo.insertAdjacentHTML('beforeend', infoAboutCat);
+            refs.divCatInfo.style.visibility = 'hidden';
+            refs.divCatInfo.style.visibility = 'visible';
+     })
 
+}
+    
+    
 
 function onFetchError(error) {
     refs.loader.style.visibility = 'hidden';
     refs.error.style.visibility = 'visible';
-
 }
